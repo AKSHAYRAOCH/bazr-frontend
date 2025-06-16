6bac1b6e03486bf7549e9a8d822261a91bb09c3d
AKSHAYRAOCH <chakshayrao@gmail.com>
2025-06-16 17:39:07 +0530

    added auth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,39 +1,29 @@
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
+import { createAuthClient } from 'better-auth/client'
 
 export const Route = createFileRoute('/')({
   component: App,
 })
 
+const authClient = createAuthClient({
+  baseURL: "http://localhost:3000"
+})
+
+const signIn = async () => {
+  const data = await authClient.signIn.social({
+    provider: "google",
+
+  })
+
+}
+
 function App() {
+
+
   return (
     <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
+      <button className='bg-black text-amber-50 ' onClick={signIn}>signin </button>
+
     </div>
   )
 }
